9ca594753e236b3614682604bfc43c76c9b44d0d
Justin <justin.ma@live.ca>
2024-01-26 03:39:12 -0500

    added route for quiz page, added styling for quiz page, added a data json file as a mock API to mimic a backend cleaned up some commented code, added new colour variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'
 import HomePage from './pages/HomePage/HomePage'
+import Quiz from './components/Quiz/Quiz'
 import './App.scss';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<HomePage />} />
-
+          <Route path='/quiz/:quizId' element={<Quiz />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,7 +1,53 @@
+import { useEffect, useState } from 'react'
+import { useParams, Navigate, useNavigate } from 'react-router-dom'
+import QuizData from '../../data/quiz-data.json'
 import './Quiz.scss'
 
 export default function Quiz() {
+    const [question, setQuestion] = useState('')
+    const [option, setOption] = useState([])
+    const [selected, setSelected] = useState('')
+    const [answer, setAnswer] = useState([])
+
+    let { quizId } = useParams()
+
+    useEffect(() => {
+        let selectedQuestion = QuizData.find(question => question.id === quizId)
+        if (selectedQuestion) {
+            setQuestion(selectedQuestion.question)
+            setOption(selectedQuestion.answers)
+            console.log(option)
+        }
+    }, [quizId])
+
+    const handleSelect = (option) => {
+        setSelected(option);
+        console.log(selected)
+    }
+
+    const navigate = useNavigate();
+
+    const handleNext = () => {
+        setAnswer((previousSelections) => [...previousSelections, selected]);
+        console.log(answer)
+        let nextQuizId = parseInt(quizId) + 1;
+        if (nextQuizId <= QuizData.length) {
+            navigate(`/quiz/${nextQuizId}`)
+        } else {
+            const delay = 2000;
+            setTimeout(() => {
+                navigate('/quiz/result')
+            }, delay);
+        }
+    }
+
     return (
-        <h2 className="quiz__question"></h2>
+        <section className="quiz">
+            <h2 className="quiz__question">{question}</h2>
+            <ul className="quiz__answer-list">{option.map((option, index) =>
+                <li key={index}><button className='quiz__button' onClick={() => handleSelect(option)} >{option}</button></li>
+            )}</ul>
+            <button className='quiz__next' onClick={handleNext} >Next</button>
+        </section>
     )
 }
