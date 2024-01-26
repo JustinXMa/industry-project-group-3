a51fae9ce6af6c20778d8c5a97fe41bc434113ee
Justin <justin.ma@live.ca>
2024-01-26 08:21:57 -0500

    updated conditional statements on quiz component to better deal with edge cases, added loading page for when quiz is finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 import HomePage from './pages/HomePage/HomePage'
 import Quiz from './components/Quiz/Quiz'
 import QuizResult from './components/QuizResult/QuizResult'
+import LoadingPage from './pages/LoadingPage/LoadingPage'
 import './App.scss';
 
 function App() {
@@ -13,8 +14,8 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/quiz/:quizId' element={<Quiz />} />
-          <Route path='/quiz/result' element={<QuizResult />} />
-
+          <Route path='/quiz/result/' element={<QuizResult />} />
+          <Route path='/loading/:selectedResult' element={<LoadingPage />} />
         </Routes>
       </BrowserRouter>
     </>
