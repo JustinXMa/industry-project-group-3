5ed73f1d37a3dcc57af7b02cdf27561a4f04a6bf
Justin <justin.ma@live.ca>
2024-01-25 22:22:56 -0500

    first draft of header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header'
 import './App.scss';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route />
 
