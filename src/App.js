3fd95543a38abf70199940c5c07ca9ecf4c39262
yacine <yacineamrouche101091@gmail.com>
2024-01-26 13:39:04 -0500

    resultpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header'
-import HomePage from './pages/HomePage/HomePage'
-import Quiz from './components/Quiz/Quiz'
-import QuizResult from './components/QuizResult/QuizResult'
-import LoadingPage from './pages/LoadingPage/LoadingPage'
-import Footer from './components/Footer/Footer';
-import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import Quiz from "./components/Quiz/Quiz";
+import QuizResult from "./components/QuizResult/QuizResult";
+import LoadingPage from "./pages/LoadingPage/LoadingPage";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
+import ResultPage from "./components/ResultPage/ResultPage";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/quiz/:quizId' element={<Quiz />} />
-          <Route path='/quiz/result/:selectedResult' element={<QuizResult />} />
-          <Route path='/loading/:selectedResult' element={<LoadingPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quiz/:quizId" element={<Quiz />} />
+          <Route path="/quiz/result/:selectedResult" element={<QuizResult />} />
+          <Route path="/loading/:selectedResult" element={<LoadingPage />} />
+          <Route path="/results" element={<ResultPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
